cc3936722e1b660bf15166d275ccb2eaaa48027b
cuiki999 <lea.opacic@gmail.com>
2019-01-25 13:49:40 +0100

    Testing if the OS can be detected

---
+++ js/get-mobile-os.js
@@ -0,0 +1,31 @@
+let compUploadDiv = document.getElementById('comp-upload');
+let phoneUploadDiv = document.getElementById('phone-upload');
+
+// mostly copied from: https://stackoverflow.com/questions/21741841/detecting-ios-android-operating-system
+function getMobileOperatingSystem() {
+  let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    compUploadDiv.style.display = 'block';
+    phoneUploadDiv.style.display = 'none';
+  }
+
+  if (/android/i.test(userAgent)) {
+    compUploadDiv.style.display = 'none';
+    phoneUploadDiv.style.display = 'block';
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    compUploadDiv.style.display = 'none';
+    phoneUploadDiv.style.display = 'block';
+  }
+
+  else {
+    compUploadDiv.style.display = 'block';
+    phoneUploadDiv.style.display = 'none';
+  }
+}
+
+getMobileOperatingSystem();
